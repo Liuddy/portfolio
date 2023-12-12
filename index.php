06b461cebcbe72b1d4548de6b48c677103eee7ab
BORDES LOME p2203124 <p2203124@univ-lyon1.fr>
2023-12-12 16:51:02 +0100

    Remplissage du head

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
     <head>
+        <meta charset="utf-8">
+        <meta name="author" content="Lomé Bordes">
+        <meta name="description" content="Portfolio">
+        <meta name="keywords" content="HTML, CSS, PHP, JavaScript, Portfolio, Lomé, Bordes, Liuddy">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="icon" href="assets/icon.png">
+		<title>Liuddy's Portfolio</title>
     </head>
     <body>
+        <header>
+ 
+        </header>
+        <main>
+
+        </main>
+        <footer>
+
+        </footer>
     </body>
 </html>
