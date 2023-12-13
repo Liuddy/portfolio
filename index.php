dd0d4e11841e864c19c6d1c9c48db4608064e43c
Liuddy <Liuddy@outlook.fr>
2023-12-13 09:17:27 +0100

    Construction de base

--- index.php
+++ index.php
@@ -8,17 +8,22 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" type="text/css" href="style.css">
 		<link rel="icon" href="assets/icon.png">
-		<title>Liuddy's Portfolio</title>
+		<title>Portfolio de Liuddy</title>
     </head>
     <body>
         <header>
- 
+            <h1>Portfolio de Liuddy</h1>
         </header>
         <main>
-
+            <p>Pr&eacutesentation du profil et du site.</p>
+            <div>
+                <img src="" alt="Visuel du projet">
+                <p>Nom du projet</p>
+                <p>Description du projet.</p>
+            </div>
         </main>
         <footer>
-
+            <p>Lom&eacute &quotLiuddy&quot Bordes &copy 2023</p>
         </footer>
     </body>
 </html>
