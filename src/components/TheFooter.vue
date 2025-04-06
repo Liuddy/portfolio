c479afbf8eeb243995bae72a2dce15fb1e7a3094
Liuddy <Liuddy@outlook.fr>
2025-04-06 20:03:11 +0200

    URL du site corrigée et nouveau build

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <footer>
-    <p>Lomé "Liuddy" Bordes &copy; 2024</p>
+    <p>Lomé "Liuddy" Bordes &copy; 2024 - {{ new Date().getFullYear() }}</p>
   </footer>
 </template>
 
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
 
-  base: '/portfolio/',
+  base: '/Portfolio/',
 
   plugins: [vue()],
 
